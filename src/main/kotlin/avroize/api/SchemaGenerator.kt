c48e4a565e4af81048d8f8e158de26241cf4a2a6
Ed Grams <edgramswork@gmail.com>
2017-11-05 14:09:56 -0600

    root node support (#3)
    * generated class path fix
    
    * jar name
    
    * override name for publish
    
    * maven local publish
    
    * uppercase first letter
    
    * root level node support
    
    * add group id for publish

--- src/main/kotlin/avroize/api/SchemaGenerator.kt
+++ src/main/kotlin/avroize/api/SchemaGenerator.kt
@@ -3,7 +3,6 @@ package avroize.api
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.array
 import com.beust.klaxon.string
-import com.google.gson.Gson
 import com.squareup.kotlinpoet.*
 import java.nio.file.Paths
 
@@ -12,13 +11,15 @@ class SchemaGenerator(val json: JsonObject) {
         val type = json.string("type")
         if (type == "record") {
             generateRecordRoot()
-        } else {
-            generateNonRecordRoot()
         }
     }
 
+    fun capitalizeFirstLetter(word: String): String {
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+
     fun generateRecordRoot() {
-        val name = json.string("name")!!
+        val name = capitalizeFirstLetter(json.string("name")!!)
         val namespace = json.string("namespace")!!
         var fileBuilder = FileSpec.builder(namespace, name)
 
@@ -29,40 +30,74 @@ class SchemaGenerator(val json: JsonObject) {
             val constructorBuilder = FunSpec.constructorBuilder()
             fields.forEach {
                 val fieldName = it.string("name")!!
-                val fieldType = it.string("type")!!
-                addConstructorParameter(classBuilder, constructorBuilder, fieldName, fieldType)
+                val type = it.get("type")
+                if (type is JsonObject) {
+                    generateRootNode(fileBuilder, fieldName, type)
+                    addConstructorParameterForClass(classBuilder, constructorBuilder, namespace, fieldName)
+                } else if (type is String) {
+                    addConstructorParameterForPrimitive(classBuilder, constructorBuilder, fieldName, type)
+                }
             }
-            classBuilder.addModifiers(KModifier.DATA)
-                    .primaryConstructor(constructorBuilder.build())
-                    .addFunction(FunSpec.builder("toJson")
-                            .addStatement("val gson = %T()", Gson::class)
-                            .addStatement("return gson.toJson(this)")
-                            .returns(String::class)
-                            .build())
+            classBuilder.primaryConstructor(constructorBuilder.build())
             fileBuilder = fileBuilder
                     .addType(classBuilder.build())
         }
 
         val record = fileBuilder.build()
-        record.writeTo(Paths.get("/home/ed/dev/avroize-api-kotlin/generated/main/kotlin"))
+
+        val currentPath = System.getProperty("user.dir")
+        record.writeTo(Paths.get(currentPath + "/generated/main/kotlin"))
     }
 
-    fun generateNonRecordRoot() {
+    fun generateRootNode(fileBuilder: FileSpec.Builder, name: String, json: JsonObject) {
+        val fields = json.array<JsonObject>("fields")
 
+        if (fields != null) {
+            val classBuilder = TypeSpec.classBuilder(capitalizeFirstLetter(name))
+            val constructorBuilder = FunSpec.constructorBuilder()
+            fields.forEach {
+                val fieldName = it.string("name")!!
+                val type = it.get("type")
+                if (type is String) {
+                    addConstructorParameterForPrimitive(classBuilder, constructorBuilder, fieldName, type)
+                }
+            }
+            classBuilder.addModifiers(KModifier.DATA).primaryConstructor(constructorBuilder.build())
+            fileBuilder.addType(classBuilder.build())
+        }
     }
 
-    fun addConstructorParameter(classBuilder: TypeSpec.Builder, constructorBuilder: FunSpec.Builder,
-                                name: String, type: String) {
+    fun addConstructorParameterForClass(classBuilder: TypeSpec.Builder, constructorBuilder: FunSpec.Builder,
+                                        namespace: String, name: String) {
+        val fullyQualifiedClassName = namespace + "." + capitalizeFirstLetter(name)
+        val className = ClassName.bestGuess(fullyQualifiedClassName)
+        constructorBuilder.addParameter(name, className)
+        classBuilder.addProperty(PropertySpec.builder(name, className)
+                .initializer(name)
+                .build())
+    }
+
+    fun addConstructorParameterForPrimitive(classBuilder: TypeSpec.Builder, constructorBuilder: FunSpec.Builder,
+                                            name: String, type: String) {
         when (type) {
-            "string" -> {
-                constructorBuilder.addParameter(name, String::class)
-                classBuilder.addProperty(PropertySpec.builder(name, String::class)
+            "boolean" -> {
+                val parameter = ParameterSpec.builder(name, Boolean::class).defaultValue("false").build()
+                constructorBuilder.addParameter(parameter)
+                classBuilder.addProperty(PropertySpec.builder(name, Boolean::class)
                         .initializer(name)
                         .build())
             }
             "int" -> {
-                constructorBuilder.addParameter(name, Int::class)
+                val parameter = ParameterSpec.builder(name, Int::class).defaultValue("0").build()
+                constructorBuilder.addParameter(parameter)
                 classBuilder.addProperty(PropertySpec.builder(name, Int::class)
+                        .initializer(name)
+                        .build())
+            }
+            "string" -> {
+                val parameter = ParameterSpec.builder(name, String::class).defaultValue("\"\"").build()
+                constructorBuilder.addParameter(parameter)
+                classBuilder.addProperty(PropertySpec.builder(name, String::class)
                         .initializer(name)
                         .build())
             }
